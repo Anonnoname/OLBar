16c81400467f41825a4f9437caf2356cf75269d0
Zander-M <zm800@nyu.edu>
2020-10-30 21:22:50 +0800

    Add google firebase & server directory

--- OLBar/Assets/Zander/Scripts/Login.cs
+++ OLBar/Assets/Zander/Scripts/Login.cs
@@ -1,18 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Net;
 
 public class Login : MonoBehaviour
 {
+    public Button loginButton;
+    public string username;
     // Start is called before the first frame update
-    void Start()
+    void GenerateRequest()
     {
-        
+        StartCoroutine(ProcessRequest());
+    }
+
+    IEnumerator ProcessRequest()
+    {
+        var input = gameObject.GetComponent<InputField>();
+
+        WWWForm form = new WWWForm();
+        form.AddField("uname", username);
+
+        using (UnityWebRequest www = UnityWebRequest.Post("https://localhost:5000/add", form)) // local server run on port 5000
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Login Success!");
+                Debug.Log(www.downloadHandler.text);
+            }
+
+
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
