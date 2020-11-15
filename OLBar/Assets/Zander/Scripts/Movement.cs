9a7267f33043947cf5465b6bb5d154f72c872088
lin <zl1836@nyu.edu>
2020-11-16 01:52:27 +0800

    drank movement code

--- OLBar/Assets/Zander/Scripts/Movement.cs
+++ OLBar/Assets/Zander/Scripts/Movement.cs
@@ -7,13 +7,62 @@ namespace OLBar
 {
     public class Movement : NetworkBehaviour
     {
+        int state;
+        float count;
+        float count2;
+        float moveX;
+        float moveY;
+        private void Start()
+        {
+            state = 1;
+        }
+
         void Update()
         {
+
+
             if (!isLocalPlayer)
                 return;
-            float moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 4.0f;
-            float moveY = Input.GetAxis("Vertical") * Time.deltaTime * 4.0f;
-            transform.Translate(moveX, moveY, 0);
+          
+          
+           
+           // if(state == 1) {
+
+     
+           // }
+
+            User user = GetComponent<User>();
+            if (user.sanity > 60) {
+                state = 1;
+            }
+            else {
+                state = 0;
+            }
+            switch (state) {
+                case 0:
+                    moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 4.0f;
+                    moveY = Input.GetAxis("Vertical") * Time.deltaTime * 4.0f;
+                    transform.Translate(moveX, moveY, 0);
+                    break;
+                case 1:
+                    count += Random.Range(0, 0.02f);
+                     moveX = Input.GetAxis("Horizontal") * Time.deltaTime * 4.0f + 0.03f * Mathf.Sin(count);
+                    count2 += Random.Range(0, 0.03f);
+                     moveY = Input.GetAxis("Vertical") * Time.deltaTime * 4.0f + 0.03f * Mathf.Sin(count2);
+                    if (count > 10) {
+                        count = 0;
+                        count2 = 0;
+                    }
+                    transform.Translate(-moveX,-moveY, 0);
+                    break;
+
+
+            }
+           
+
+
+
         }
+
     }
 }
