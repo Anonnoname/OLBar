e7e7ba668ccccff83c7e2aaf6d0ba93bbf6373f6
Zander-M <zm800@nyu.edu>
2020-12-01 18:49:26 +0800

    create jukebox

--- OLBar/Assets/Zander/Scripts/User.cs
+++ OLBar/Assets/Zander/Scripts/User.cs
@@ -33,12 +33,12 @@ namespace OLBar
         // chatwindow
 
 
-        public override void OnStartLocalPlayer() 
+        public override void OnStartLocalPlayer()
         {
             Camera.main.transform.SetParent(transform);
             Camera.main.transform.localPosition = new Vector3(0, 0, -10); // set main camera position
-	
-            
+
+
             // initialize user
             this.currency = 1000;
             this.sanity = 100;
@@ -66,6 +66,7 @@ namespace OLBar
             m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_ColorGrading, m_Vignette, m_LensDistortion);
             InvokeRepeating("Tired", 5.0f, 0.0001f);
         }
+
 
         private void Tired()
         {
