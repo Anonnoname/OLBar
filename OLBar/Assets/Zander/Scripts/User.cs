40d63683237ccdac7a1762c5dc4a0c48e263057d
Zander-M <zm800@nyu.edu>
2020-12-03 13:50:44 +0800

    fix bug

--- OLBar/Assets/Zander/Scripts/User.cs
+++ OLBar/Assets/Zander/Scripts/User.cs
@@ -1,7 +1,6 @@
 using System;
 using Mirror;
 using UnityEngine;
-using UnityEngine.Rendering.PostProcessing;
 
 
     public class User: NetworkBehaviour
@@ -24,10 +23,7 @@ using UnityEngine.Rendering.PostProcessing;
         public GameObject chatBox; // chatbox
 
         // VISUAL EFFECTS
-        PostProcessVolume m_Volume; // post processing filter
-        Vignette m_Vignette;
-        ColorGrading m_ColorGrading;
-        LensDistortion m_LensDistortion;
+
 
         // chatwindow
 
@@ -49,20 +45,8 @@ using UnityEngine.Rendering.PostProcessing;
             window.localUser = this;
 
             // setup visual effects
-            m_Vignette = ScriptableObject.CreateInstance<Vignette>();
-            m_Vignette.enabled.Override(true);
-            m_Vignette.intensity.Override(0f);
 
-            m_ColorGrading = ScriptableObject.CreateInstance<ColorGrading>();
-            m_ColorGrading.enabled.Override(true);
-            m_ColorGrading.saturation.Override(0f);
 
-            m_LensDistortion = ScriptableObject.CreateInstance<LensDistortion>();
-            m_LensDistortion.enabled.Override(true);
-            m_LensDistortion.intensity.Override(0f);
-            m_LensDistortion.scale.Override(1f);
-
-            m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_ColorGrading, m_Vignette, m_LensDistortion);
             InvokeRepeating("Tired", 5.0f, 0.0001f);
         }
 
@@ -81,9 +65,7 @@ using UnityEngine.Rendering.PostProcessing;
 
         void Update()
         {
-                m_Vignette.intensity.value = sleepiness;
-                m_ColorGrading.saturation.value = -hunger;
-                m_LensDistortion.intensity.value = Mathf.Sin(Time.realtimeSinceStartup) * (100 - sanity);
+
         }
 
 
---
+++ OLBar/Assets/Zander/Scripts/VFX.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class VFX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    User user;
+    PostProcessVolume m_Volume; // post processing filter
+    Vignette m_Vignette;
+    ColorGrading m_ColorGrading;
+    LensDistortion m_LensDistortion;
+    void Start()
+    {
+        user = GetComponent<User>();
+        if (user.isLocalPlayer)
+        {
+            return;
+        }
+        m_Vignette = ScriptableObject.CreateInstance<Vignette>();
+        m_Vignette.enabled.Override(true);
+        m_Vignette.intensity.Override(0f);
+
+        m_ColorGrading = ScriptableObject.CreateInstance<ColorGrading>();
+        m_ColorGrading.enabled.Override(true);
+        m_ColorGrading.saturation.Override(0f);
+
+        m_LensDistortion = ScriptableObject.CreateInstance<LensDistortion>();
+        m_LensDistortion.enabled.Override(true);
+        m_LensDistortion.intensity.Override(0f);
+        m_LensDistortion.scale.Override(1f);
+
+        m_Volume = PostProcessManager.instance.QuickVolume(gameObject.layer, 100f, m_ColorGrading, m_Vignette, m_LensDistortion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (user.isLocalPlayer)
+        {
+            return;
+        }
+        m_Vignette.intensity.value = user.sleepiness;
+        m_ColorGrading.saturation.value = -user.hunger;
+        m_LensDistortion.intensity.value = Mathf.Sin(Time.realtimeSinceStartup) * (100 - user.sanity);
+    }
+}
